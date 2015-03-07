f4ad1b5f82b5be7fd2362498e91f5babc8209292
robby <robertapolana@gmail.com>
2015-03-07 18:31:11 +0700

    :sunglasses:
    Fix for:
    
    https://github.com/hughsk/vinyl-transform/issues/5

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 
 
 
-gulp.task('pure-browserify', ['clean'], function () {
+gulp.task('pure-browserify', ['clean'], function() {
   return browserify('./index.coffee')
     .transform('coffeeify')
     .bundle()
@@ -25,8 +25,8 @@ gulp.task('pure-browserify', ['pure-browserify'], openTask);
 gulp.task('bundle-coffee-entry', ['clean'], function() {
   return gulp.src('./index.coffee')
     .pipe(gBrowserify({
-      transform:['coffeeify']
-    }))
+    transform: ['coffeeify']
+  }))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('./'))
 });
@@ -38,8 +38,8 @@ gulp.task('coffee-entry', ['bundle-coffee-entry'], openTask);
 gulp.task('bundle-js-entry', ['clean'], function() {
   return gulp.src('./index.js')
     .pipe(gBrowserify({
-      transform:['coffeeify']
-    }))
+    transform: ['coffeeify']
+  }))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('./'))
 });
@@ -49,23 +49,34 @@ gulp.task('js-entry', ['bundle-js-entry'], openTask);
 
 
 gulp.task('bundle-vinyl-transform', ['clean'], function() {
-  var browserified = transform(function(filename){
-     return browserify(filename)
-       .transform('coffeeify')
-       .bundle();
-  });
+
+  var through = require('through2');
+
+  var browserified = function(file, env, cb) {
+    var self = this;
+    return browserify(file)
+      .transform('coffeeify')
+      .bundle(function(err, src) {
+      if (err) {
+        self.emit('error', err)
+      } else {
+        file.contents = new Buffer(src);
+        self.push(file);
+      }
+      cb();
+    })
+  };
 
   return gulp.src('./index.js')
-    .pipe(browserified)
+    .pipe(through.obj(browserified))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('.'));
 });
+
 gulp.task('vinyl-transform', ['bundle-vinyl-transform'], openTask);
 
 
-
-
-gulp.task('bundle-vinyl-source-stream', ['clean'], function () {
+gulp.task('bundle-vinyl-source-stream', ['clean'], function() {
   return browserify('./index.coffee')
     .transform('coffeeify')
     .bundle()
@@ -77,8 +88,10 @@ gulp.task('vinyl-source-stream', ['bundle-vinyl-source-stream'], openTask);
 
 
 
-gulp.task('clean', function(){
-  return gulp.src('./bundle.js', {read: false})
+gulp.task('clean', function() {
+  return gulp.src('./bundle.js', {
+    read: false
+  })
     .pipe(clean());
 });
 
